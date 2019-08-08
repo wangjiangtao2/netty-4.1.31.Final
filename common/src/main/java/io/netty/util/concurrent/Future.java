e808cc633fae4566138d5e71dab7c521750557bb
sjt <18030440+wangjiangtao2@users.noreply.github.com>
2019-08-08 12:37:47 +0800

    11

--- common/src/main/java/io/netty/util/concurrent/Future.java
+++ common/src/main/java/io/netty/util/concurrent/Future.java
@@ -21,6 +21,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * The result of an asynchronous operation.
+ * 获取异步操作的结果
  */
 @SuppressWarnings("ClassNameSameAsAncestorName")
 public interface Future<V> extends java.util.concurrent.Future<V> {
